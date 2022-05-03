7643cb4b08b0299d2b8ec0605f537f2a23ea5753
萌萌四 <1239612746@qq.com>
2022-05-03 15:53:41 +0800

    update version of nep cpu

--- nep_cpu/src/pynep.cpp
+++ nep_cpu/src/pynep.cpp
@@ -1,28 +1,4 @@
-/*
-    Copyright 2017 Zheyong Fan, Ville Vierimaa, Mikko Ervasti, and Ari Harju
-    This file is part of GPUMD.
-    GPUMD is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    GPUMD is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*----------------------------------------------------------------------------80
-Usage:
-    Compile:
-        g++ -O3 main.cpp nep.cpp
-    run:
-        ./a.out
-------------------------------------------------------------------------------*/
-
 #include "nep.h"
-#include "utility.h"
 #include <cmath>
 #include <fstream>
 #include <iomanip>
@@ -33,12 +9,10 @@ Usage:
 #include <pybind11/stl.h>
 namespace py = pybind11;
 
-const int MN = 1000;
-
 struct Atom {
   int N;
-  std::vector<int> num_cells, type, NN_radial, NL_radial, NN_angular, NL_angular;
-  std::vector<double> box, ebox, position, r12, potential, force, virial;
+  std::vector<int> type;
+  std::vector<double> box, position, potential, force, virial;
 };
 
 class NepCalculator
@@ -70,20 +44,10 @@ void NepCalculator::setAtoms(
 {
   Atom _atom;
   _atom.N = _N;
-  _atom.num_cells.resize(3);
   _atom.box = _box;
-  _atom.box.resize(18);
-  _atom.ebox.resize(18);
-  get_inverse(_atom.box.data());
-
   _atom.type = _type;
   _atom.position = _position;
 
-  _atom.NN_radial.resize(_atom.N);
-  _atom.NL_radial.resize(_atom.N * MN);
-  _atom.NN_angular.resize(_atom.N);
-  _atom.NL_angular.resize(_atom.N * MN);
-  _atom.r12.resize(_atom.N * MN * 6);
   _atom.position.resize(_atom.N * 3);
   _atom.potential.resize(_atom.N);
   _atom.force.resize(_atom.N * 3);
@@ -94,14 +58,7 @@ void NepCalculator::setAtoms(
 
 void NepCalculator::calculate()
 {
-  find_neighbor_list_small_box(
-    calc.paramb.rc_radial, calc.paramb.rc_angular, atom.N, atom.box, atom.position,
-    atom.num_cells, atom.ebox, atom.NN_radial, atom.NL_radial, atom.NN_angular, atom.NL_angular,
-    atom.r12);
-
-  calc.compute(
-    atom.NN_radial, atom.NL_radial, atom.NN_angular, atom.NL_angular, atom.type, atom.r12,
-    atom.potential, atom.force, atom.virial);
+  calc.compute(atom.type, atom.box, atom.position, atom.potential, atom.force, atom.virial);
 }
 
 std::vector<double> NepCalculator::getPotentialEnergy()
